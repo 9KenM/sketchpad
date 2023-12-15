7a94389199a61fcf8717218b516efe63394de44f
Ken <dev@MacBook-Air.local>
2023-12-15 15:58:24 -0500

    Added random color palette

--- public/sketchpad/sketchpad.js
+++ public/sketchpad/sketchpad.js
@@ -1,6 +1,6 @@
 const Sketchpad = (() => {
     let currPath;
-    let currColor = "black";
+    let currColor = "000000";
     let isDrawing = false;
     let pathData = "";
     let drawSurface;
@@ -10,105 +10,130 @@ const Sketchpad = (() => {
         drawSurface = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         drawSurface.classList.add("sketchpad_drawSurface");
         drawSurface.setAttribute("width", "100%");
-        drawSurface.setAttribute("height", "100%");
-        el.appendChild(drawSurface);
-    }
-
-    function createPath() {
-        let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("stroke", currColor);
-        path.setAttribute("fill", "none");
-        path.setAttribute("stroke-linecap", "round");
-        path.setAttribute("stroke-linejoin", "round");
-        path.setAttribute("stroke-width", "5");
-        path.classList.add("sketchpad_drawPath");
-        return path;
-    }
-        
-    function startDrawing(event) {
-        isDrawing = true;
-        currPath = createPath();
-        drawSurface.appendChild(currPath);  
-        pathData = `M ${event.clientX} ${event.clientY} L ${event.clientX} ${event.clientY}`;
-        currPath.setAttribute("d", pathData);
-    }
-    
-    function draw(event) {
-      if (!isDrawing) return;
-      pathData += ` L ${event.clientX} ${event.clientY}`;
-      currPath.setAttribute("d", pathData);
-    }
-    
-    function endDrawing() {
-      isDrawing = false;
-      currPath = undefined;
-    }
-        
-    function undo() {
-        let paths = Array.from(drawSurface.getElementsByClassName("sketchpad_drawPath"));
-        if (paths.length > 0) {
-            let lastPath = paths.pop();
-            lastPath.parentNode.removeChild(lastPath);
+            drawSurface.setAttribute("height", "100%");
+            el.appendChild(drawSurface);
         }
-    }
 
-    function clear() {
-        drawSurface.innerHTML = "";
-    }
-    
-    function setColor(color) {
-        currColor = color;
-    }
+        function createPath() {
+            let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            path.setAttribute("stroke", "#"+currColor);
+            path.setAttribute("fill", "none");
+            path.setAttribute("stroke-linecap", "round");
+            path.setAttribute("stroke-linejoin", "round");
+            path.setAttribute("stroke-width", "5");
+            path.classList.add("sketchpad_drawPath");
+            return path;
+        }
+            
+        function startDrawing(event) {
+            isDrawing = true;
+            currPath = createPath();
+            drawSurface.appendChild(currPath);  
+            pathData = `M ${event.clientX} ${event.clientY} L ${event.clientX} ${event.clientY}`;
+            currPath.setAttribute("d", pathData);
+        }
+        
+        function draw(event) {
+          if (!isDrawing) return;
+          pathData += ` L ${event.clientX} ${event.clientY}`;
+          currPath.setAttribute("d", pathData);
+        }
+        
+        function endDrawing() {
+          isDrawing = false;
+          currPath = undefined;
+        }
+            
+        function undo() {
+            let paths = Array.from(drawSurface.getElementsByClassName("sketchpad_drawPath"));
+            if (paths.length > 0) {
+                let lastPath = paths.pop();
+                lastPath.parentNode.removeChild(lastPath);
+            }
+        }
 
-    function loadCSS(path) {
-        const oldLink = document.querySelector(`link[href="${path}"]`);
-        if(oldLink) oldLink.parentNode.removeChild(oldLink);
+        function clear() {
+            drawSurface.innerHTML = "";
+        }
+        
+        function setColor(color) {
+            currColor = color;
+        }
 
-        const cssLink = document.createElement("link");
-        cssLink.rel = "stylesheet";
-        cssLink.href = path + '?' + new Date().getTime();
-        document.head.appendChild(cssLink);
-    }
+        function loadCSS(path) {
+            const oldLink = document.querySelector(`link[href="${path}"]`);
+            if(oldLink) oldLink.parentNode.removeChild(oldLink);
 
-    function init(containerEl) {
-        if(!containerEl) return;
-        loadCSS("./sketchpad/style.css");
-        containerEl.style.position = "relative";
-        containerEl.style.display = "flex";
-        createDrawSurface(containerEl);
-        containerEl.addEventListener("pointerdown", startDrawing);
-        document.addEventListener("pointermove", draw);
-        document.addEventListener("pointerup", endDrawing);
-    }
+            const cssLink = document.createElement("link");
+            cssLink.rel = "stylesheet";
+            cssLink.href = path + '?' + new Date().getTime();
+            document.head.appendChild(cssLink);
+        }
 
-    function download() {
-        const svg = formatSvg(drawSurface.innerHTML);
-        let parser = new DOMParser();
-        let blob = new Blob([svg], {type: "image/svg+xml;charset=utf-8"});
-        triggerDownload(URL.createObjectURL(blob));
-    }
+        function init(containerEl) {
+            if(!containerEl) return;
+            loadCSS("./sketchpad/style.css");
+            containerEl.style.position = "relative";
+            containerEl.style.display = "flex";
+            createDrawSurface(containerEl);
+            containerEl.addEventListener("pointerdown", startDrawing);
+            document.addEventListener("pointermove", draw);
+            document.addEventListener("pointerup", endDrawing);
+        }
 
-    function formatSvg(rawData) {
-        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1">${rawData}</svg>`;
-    }
+        function download() {
+            const svg = formatSvg(drawSurface.innerHTML);
+            let parser = new DOMParser();
+            let blob = new Blob([svg], {type: "image/svg+xml;charset=utf-8"});
+            triggerDownload(URL.createObjectURL(blob));
+        }
 
-    function triggerDownload(dataUrl) {
-        let downloadLink = document.createElement("a");
-        downloadLink.href = dataUrl;
-        downloadLink.download = `sketchpad-${new Date().getTime()}.svg`;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-    }
+        function formatSvg(rawData) {
+            return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1">${rawData}</svg>`;
+        }
 
-    return {
-        init: init,
-        undo: undo,
-        setColor: setColor,
-        clear: clear,
-        download: download
-    }
+        function triggerDownload(dataUrl) {
+            let downloadLink = document.createElement("a");
+            downloadLink.href = dataUrl;
+            downloadLink.download = `sketchpad-${new Date().getTime()}.svg`;
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+        }
 
-})();
+        function rgbToHex(rgb) {
+            const [r, g, b] = rgb;
+            const hex = ((r << 16) | (g << 8) | b).toString(16).padStart(6, '0');
+            return `${hex}`;
+        }
 
-export default Sketchpad;
+        async function getRandomPalette() {
+            const response = await fetch('http://colormind.io/api/', {
+                method: 'POST',
+                body: JSON.stringify({
+                    model: "default",
+                })
+            });
+        
+            const data = await response.json();
+            return data.result.map(rgb => rgbToHex(rgb));
+        }
+
+        // async function getRandomPalette() {
+        //     const response = await fetch(`http://www.thecolorapi.com/scheme?hex=${getRandomColor()}&mode=analogic`);
+        //     const data = await response.json();
+        //     return data.colors.map(color => color.hex.value);
+        // }
+
+        return {
+            init: init,
+            undo: undo,
+            setColor: setColor,
+            clear: clear,
+            download: download,
+            getRandomPalette: getRandomPalette
+        }
+
+    })();
+
+    export default Sketchpad;
